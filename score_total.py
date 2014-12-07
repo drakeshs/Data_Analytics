119dc2ea04fb70f576d8ba6d292bd66b58202eba
S. Lopez de Diego <slopezd09@gmail.com>
2014-12-07 16:00:07 -0500

    generates a file "total.txt" with the employee ID in the first column and the final score in the second column

--- score_total.py
+++ score_total.py
@@ -11,6 +11,8 @@ import os
 import sys
 import shutil
 
+#***************************************************************************
+
 shutil.copy('Employee_Info_sub.csv', 'Database/Employee_Info_sub.csv')
 os.chdir('Database')
 if os.path.exists('score_total.txt'):
@@ -19,6 +21,12 @@ if os.path.exists('sum_file.txt'):
     os.remove('sum_file.txt')
 if os.path.exists('score_total.txt'):
     os.remove('score_total.txt')
+if os.path.exists('score_total1.txt'):
+    os.remove('score_total1.txt')
+if os.path.exists('total.txt'):
+    os.remove('total.txt')
+
+#**************************************************************************
 #with open ('score_acivity.txt' , 'rb') as fil:     #Opens the CSV file
  #   for col in (row.strip().split() for row in fil):
 
@@ -73,7 +81,71 @@ with open('sum_file.txt', 'rb') as f:
         sys.stdout = orig_stdout
         out.close()
 
+
+#***********************************************************************************
+#generating file with employee info included
+#***********************************************************************************
+
+with open ('Employee_Info_sub.csv', 'rb') as csvfile:     #Opens the CSV file
+    employee_info = csv.reader(csvfile, delimiter=',') #Stores the fileds in a variable 'employee_info'
+    next(employee_info, None)              #Skips the first line of the CSV (Headers)
+    for col in employee_info:
+        emp_id = col[0].strip()
+        orig_stdout = sys.stdout 
+        out = file('score_total1.txt', 'a')
+        sys.stdout = out
+        print emp_id
+        sys.stdout = orig_stdout
+        out.close()
+
+#with open ('score_acivity.txt' , 'rb') as fil:     #Opens the CSV file
+ #   for col in (row.strip().split() for row in fil):
+
+
+#with open ('score_info.txt', 'rb') as f:
+ #   for cols in (row.strip().split() for row in f):
+  #      score_info = int(cols[2])
+
+# List of your files
+file_names = ['score_total1.txt', 'score_total.txt']
+
+# Output list of generator objects
+o_data = []
+
+# Open files in the succession and 
+# store the file_name as the first
+# element followed by the elements of
+# the third column.
+for afile in file_names:
+    file_h = open(afile)
+    a_list = []
+    a_list.append(afile)
+    csv_reader = csv.reader(file_h, delimiter=' ')
+    for row in csv_reader:
+        a_list.append(row[0])
+    # Convert the list to a generator object
+    o_data.append((n for n in a_list))
+    file_h.close()
+
+# Use zip and csv writer to iterate
+# through the generator objects and 
+# write out to the output file
+with open('total.txt', 'w') as op_file:
+    csv_writer = csv.writer(op_file, delimiter=',')
+    for row in list(zip(*o_data)):
+        csv_writer.writerow(row)
+op_file.close()
+
+#******************************************************************************
+#Cleans directory
+#*****************************************************************************
+
 if os.path.exists('Employee_Info_sub.csv'):
-        os.remove('Employee_Info_sub.csv')
+    os.remove('Employee_Info_sub.csv')
 if os.path.exists('sum_file.txt'):
     os.remove('sum_file.txt')
+if os.path.exists('score_total.csv'):
+    os.remove('score_total.csv')
+if os.path.exists('score_total1.txt'):
+    os.remove('score_total1.txt')
+
